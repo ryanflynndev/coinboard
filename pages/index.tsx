5155bf0354e6ed1e69c5ab1b76d818cbf1c47fae
Ryan Flynn <ryanflynn44@gmail.com>
2022-05-15 17:05:09 -0400

    Added formatting for dollars and percentages

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,22 @@ const coinDataStyle = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
 
 const Home: NextPage = (props: any) => {
   const { data } = props.result;
+
+  const formatPercent = (number: any) => {
+    return `${new Number(number).toFixed(2)}%`;
+  };
+
+  const formatDollar = (number: any, maximumSignificantDigits: any) => {
+    return new Intl.NumberFormat(
+      'en-US',
+      {
+        style: 'currency',
+        currency: 'usd',
+        maximumSignificantDigits
+      }
+    ).format(number);
+  }
+
   return (
     <div>
       <Head>
@@ -21,10 +37,9 @@ const Home: NextPage = (props: any) => {
             <table className="min-w-full">
               <thead>
                 <tr>
-                  <th className={tableHeadStyle}>Image</th>
                   <th className={tableHeadStyle}>Symbol</th>
-                  <th className={tableHeadStyle}>24H Change</th>
                   <th className={tableHeadStyle}>Price</th>
+                  <th className={tableHeadStyle}>24H Change</th>
                   <th className={tableHeadStyle}>Market Cap</th>
                 </tr>
               </thead>
@@ -34,15 +49,19 @@ const Home: NextPage = (props: any) => {
                   return <tr key={coin.id}>
                     <td className={coinDataStyle}>
                       <div className="flex items-center">
-                        <div className="flex-shrink-0 w-10 h-10">
+                        <div className="flex-shrink-0 w-10 h-10 flex items-center">
                           <img className="w-10 h-10 rounded-full" src={coin.image}></img>
+                          <div className="px-5">{coin.symbol.toUpperCase()}</div>
                         </div>
                       </div>
                     </td>
-                    <td className={coinDataStyle}>{coin.symbol.toUpperCase()}</td>
-                    <td className={coinDataStyle}>{coin.price_change_percentage_24h}</td>
-                    <td className={coinDataStyle}>{coin.current_price}</td>
-                    <td className={coinDataStyle}>{coin.market_cap}</td>
+                    <td className={coinDataStyle}>{formatDollar(coin.current_price, 20)}</td>
+                    <td className={coinDataStyle}>
+                      <span className={coin.price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'}>
+                        {formatPercent(coin.price_change_percentage_24h)}
+                      </span>
+                    </td>
+                    <td className={coinDataStyle}>{formatDollar(coin.market_cap, 12)}</td>
                   </tr>
                 })}
               </tbody>
